7aaeb4884af811d0a61421601b1241220f56f1f8
NowrinIslamMim <nowrin02@gmail.com>
2022-10-11 05:15:17 +0600

    netlify react router 404 solved

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -3,7 +3,33 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h2>Blog</h2>
+            <div class="card text-center mt-5 w-50 mx-auto">
+                <div class="card-header fw-bold fs-4">
+                    What is the purpose of React router?
+                </div>
+                <div class="card-body">
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                <div class="card-footer text-muted"></div>
+            </div>
+            <div class="card text-center mt-5 w-50 mx-auto">
+                <div class="card-header fw-bold fs-4">
+                    How does Context API work?
+                </div>
+                <div class="card-body">
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                <div class="card-footer text-muted"></div>
+            </div>
+            <div class="card text-center mt-5 w-50 mx-auto">
+                <div class="card-header fw-bold fs-4">
+                    Describe about 'useRef' hook.
+                </div>
+                <div class="card-body">
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                <div class="card-footer text-muted"></div>
+            </div>
         </div>
     );
 };
