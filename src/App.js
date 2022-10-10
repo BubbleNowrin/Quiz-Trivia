9dedaed572e3ec7448c8536d92f4ec58929252fb
NowrinIslamMim <nowrin02@gmail.com>
2022-10-10 21:18:59 +0600

    router created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from '../src/Layouts/Main'
+import Quizzes from './Components/Quizzes/Quizzes';
+import Statistics from './Components/Statistics/Statistics';
+import Blog from './Components/Blog/Blog';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Quizzes></Quizzes>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics> </Statistics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Components/Quizzes/Quizzes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Quizzes = () => {
+    return (
+        <div>
+            <h2>This is quizzes Component</h2>
+        </div>
+    );
+};
+
+export default Quizzes;
